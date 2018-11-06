cd19b569ba2dd59dc94748cd0d94f2987e77c88c
Philipp <philippboensch@gmail.com>
2018-11-06 22:26:56 +0100

    cmake updated

--- shader/vertex.glsl
+++ shader/vertex.glsl
@@ -1,11 +1,12 @@
 #version 450 core
 
 uniform mat4 mvp;
-layout(location = 0) in vec2 position;
-layout(location = 1) in vec2 uv;
+layout (location = 0) in vec2 position;
+layout (location = 1) in vec2 uv;
 out vec2 varriant_uv;
 
-void main() {
+void main()
+{
     gl_Position = mvp * vec4(position, 0, 1);
     varriant_uv = uv;
 }
